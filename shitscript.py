9866b4707f7f1e9a467d566dce4e73afc1fe7064
nyuhanc <pavsek.aljaz@gmail.com>
2023-06-27 14:29:59 +0200

    commit003

--- shitscript.py
+++ shitscript.py
@@ -1,3 +1,37 @@
+import numpy as np
+import scipy.signal
+import matplotlib.pyplot as plt
+
+# suppose `a` and `b` are your numpy arrays
+a = np.random.random(1000) - 0.5
+b = np.random.random(1000) - 0.5
+
+# calculate cross-correlation
+xcorr = scipy.signal.correlate(a, b, mode='full')
+
+# calculate the number of overlapping points at each lag
+overlap = np.correlate(np.ones_like(a), np.ones_like(b), mode='full')
+
+# normalize cross-correlation by the overlap
+xcorr_normalized = xcorr / overlap
+
+# compute significance level
+N = len(a)  # number of samples
+stat_sig = 2 / np.sqrt(N)
+
+# plot cross-correlation and significance level
+lags = np.arange(-N + 1, N)  # array of lags
+plt.plot(lags, xcorr_normalized)
+plt.plot(lags, [stat_sig]*len(lags), 'r')
+plt.plot(lags, [-stat_sig]*len(lags), 'r')
+plt.show()
+
+
+
+
+
+
+
 # filter out noise  with Butterworth filter
 cutoff = 0.05
 order = 10
